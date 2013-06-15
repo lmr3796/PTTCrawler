7801b63fedafa66133bfc18b204e388588987e3d
lmr3796 <lmr3796joseph@gmail.com>
2013-06-15 22:27:56 +0800

    Can goto certain board

--- pttcrawler.rb
+++ pttcrawler.rb
@@ -29,11 +29,21 @@ class PTTCrawler
         end
         @tn.cmd('Match' => Regexp.new("批踢踢實業坊".force_encoding('binary')), 'String' => ''){|s| puts s}
     end
+    def goto_board(board_name)
+        @tn.puts("s#{board_name}")
+        begin
+            @tn.waitfor('Match' => Regexp.new("看板《".force_encoding('binary'))){|s| puts s}
+        rescue
+            @tn.puts('')
+            retry
+        end
+    end
 end
 
 
-crawler = PTTCrawler.new(:host => 'ptt.cc', :username => argv[1], :password => argv[2])
+crawler = PTTCrawler.new(:host => 'ptt.cc', :username => ARGV[0], :password => ARGV[1])
 crawler.login
+crawler.goto_board('gossiping')
 
 
 
